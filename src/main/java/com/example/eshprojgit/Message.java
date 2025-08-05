90db76de73cb256a8f67ba7d728f04e7cfeb0f98
agrawaleshika07-dev <agrawal.eshika07@gmail.com>
2025-08-05 23:57:53 +0530

    Initial commit from ServiceNow

---
+++ src/main/java/com/example/eshprojgit/Message.java
@@ -0,0 +1,32 @@
+package com.example.eshprojgit;
+
+import javax.persistence.*;
+
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    public Message() {}
+
+    public Message(String content) {
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
+
